85332d183ad9e8636618a3ed40aeda37b51143f7
chenglu <chenglu.she@gmail.com>
2019-10-10 12:39:32 +0800

    change version

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ import setuptools
 setuptools.setup(
     name='featurize-package',
     description='Official packages for featurize.',
-    version='0.0.2',
+    version='0.0.3',
     packages=setuptools.find_packages(),
     url="https://github.com/louis-she/featurize-package",
     author='louis',
     author_email='chenglu.she@gmail.com',
     keywords='pytorch minecraft',
-    package_data={'ftpkg': ['*']}
+    include_package_data=True,
     # install_requires=required,
     # entry_points={
     #     'console_scripts': ['minetorch=minetorch.cli:cli'],
