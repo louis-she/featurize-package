da6331bb8cd3b46f0b53b12f386183758fe0afdc
chenglu <chenglu.she@gmail.com>
2019-10-14 17:12:19 +0800

    add ftpkg.ssdd

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-#
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
-
 
 setuptools.setup(
     name='featurize-package',
     description='Official packages for featurize.',
-    version='0.0.5',
+    version='0.0.6',
     packages=setuptools.find_packages(),
     url="https://github.com/louis-she/featurize-package",
     author='louis',
     author_email='chenglu.she@gmail.com',
     keywords='pytorch minecraft',
     include_package_data=True,
-    install_requires=['torchvision>=0.4.0'],
-    # entry_points={
-    #     'console_scripts': ['minetorch=minetorch.cli:cli'],
-    # }
+    install_requires=[
+        'torchvision>=0.4.0',
+        'albumentations',
+        'torch',
+        'pandas',
+        'numpy',
+        'opencv-python',
+        'scikit-learn',
+        'pretrainedmodels',
+        'kaggle'
+    ],
 )
