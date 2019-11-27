2f090b3fb80bf177a1404740116d3c9823c1d129
pengbo0054 <pengbo0054@gmail.com>
2019-11-27 19:36:55 +0800

    fix bugs

--- ftpkg/models/__init__.py
+++ ftpkg/models/__init__.py
@@ -8,13 +8,13 @@ encoder_weights_collection = [('random', None), 'imagenet']
 encoder_name_collection = ['resnet34', 'resnet50', 'resnet101']
 activation_collection = ['None', 'sigmoid', 'softmax']
 
-encoder_name = Option(default='resnet34', type='collection', collection=encoder_name_collection)
-encoder_weights = Option(default='imagenet', type='collection', collection=encoder_weights_collection)
-num_classes = Option(default=2, type='number', help='class number of mask')
-activation = Option(default='None', type='collection', collection=activation_collection)
-
 class FPNResnet34(Model):
-
+    
+    encoder_name = Option(default='resnet34', type='collection', collection=encoder_name_collection)
+    encoder_weights = Option(default='imagenet', type='collection', collection=encoder_weights_collection)
+    num_classes = Option(default=2, type='number', help='class number of mask')
+    activation = Option(default='None', type='collection', collection=activation_collection)
+    
     def __call__(self):
         model = smp.FPN(
             encoder_name=self.encoder_name, 
