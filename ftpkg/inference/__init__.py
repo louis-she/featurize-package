ac4ec2a5ef67a7491c6d884d06748b813675a137
pengbo0054 <pengbo0054@gmail.com>
2019-11-27 18:00:26 +0800

    update visualize package

--- ftpkg/inference/__init__.py
+++ ftpkg/inference/__init__.py
@@ -4,43 +4,31 @@ import minetorch
 
 
 class MixinMeta():
-    namespace = 'minetorch'
-
-
-class CorePlugin(minetorch.Plugin):
-    """The Minetorch Trainer can be runned independently.
-    This plugin activate Trainer with the ability to communicate with the
-    Minetorch Server with some basic data collection such as loss.
-    """
-    def after_init(self, payload):
-        env.rpc.create_graph('train_epoch_loss')
-        env.rpc.create_graph('val_epoch_loss')
-        env.rpc.create_graph('train_iteration_loss')
-
-    def after_epoch_end(self, payload):
-        env.rpc.add_point('train_epoch_loss', payload['epoch'], payload['train_loss'])
-        env.rpc.add_point('val_epoch_loss', payload['epoch'], payload['val_loss'])
-
-    def after_checkpoint_persisted(self, payload):
-        env.rpc.add_file(payload['modelpath'])
-
-    def after_train_iteration_end(self, payload):
-        env.rpc.add_point('train_iteration_loss', payload['iteration'], payload['loss'])
-
+    namespace = 'inference'
 
 
 class Inference(Task, MixinMeta):
-    uploaded_images = Option(type='upload')
-    image_height = Option(type='number', required=True)
-    image_width = Option(type='number', required=True)
+    output_activation = Option(name='activation', type='collection', default='None', collection=[['None', 'sigmoid', 'softmax']])
+    pixel_threshold = Option(name='Pixel threshold', type='number', default='0.5')
+    dataset = BasicModule(name='Dataset', component_types=['Dataset'])
     transform = DataflowModule(name='Transform', component_types=['Dataflow'], multiple=True, required=False)
     model = BasicModule(name='Model', component_types=['Model'])
-    output_activation = Option(name='activation', type='collection', default='None', collection=[['None', 'sigmoid', 'softmax']])
-
+    
+    def mask2rle(image_logits, pixel_threshold):
+        img = image_logits > pixel_threshold
+        pixels= img.T.flatten()
+        pixels = np.concatenate([[0], pixels, [0]])
+        runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+        runs[1::2] -= runs[::2]
+        return ' '.join(str(x) for x in runs)
+   
     def __call__(self):
-        inference_images = [cv2.imread(img for img in self.uploaded_images)]
+        fnames = [i.split('/')[-1] for i in self.uploaded_images]
+        inference_images = self.dataset
         inputs = [self.transform(image) for image in inference_images]
         outputs = [self.model(input.unsqueeze(0)).squeeze() for input in inputs]
+        classes = outputs[0].shape[0]
+
         if self.output_activation == 'None':
             results = outputs
         elif self.output_activation == 'sigmoid':
@@ -49,10 +37,20 @@ class Inference(Task, MixinMeta):
             results = [torch.nn.Softmax(dim=0)(output) for output in outputs]
         else:
             env.logger.exception(f'unexpected error in inferencing process.')
-        fnames = [i.split('/')[-1] for i in self.uploaded_images]
-        tmp = []
+        
+        encoded_masks = []
         for fname, result in zip(fnames, results):
-            tmp.append((fname,result))
-        df = pd.DataFrame(tmp, columns=['fname', 'predictions'])
+            encoded_mask = []
+            encoded_mask.append(fname)
+            for i, mask in enumerate(result):
+                encoded_mask.append(mask2rle(mask, self.pixel_threshold))
+            encoded_masks.append(encoded_mask)
+        
+        columns = []
+        columns.append('image_names')
+        for i in range(classes):
+            columns.append(f'class_{i+1}')
+        
+        df = pd.DataFrame(encoded_masks, columns=columns)
         df.to_csv('./submission.csv', index=False)
         env.rpc.add_file('./submission.csv')
---
+++ ftpkg/visualize/__init__.py
@@ -0,0 +1,81 @@
+import minetorch
+
+import matplotlib.patches as mpatches
+
+from featurize_jupyterlab.core import Task, BasicModule, DataflowModule, Option
+from featurize_jupyterlab.task import env
+from PIL import Image, ImageDraw, ImageFont
+
+
+colors = [
+    (255,255,0),
+    (0,255,255),
+    (255,0,255),
+    (0,0,255),
+    (0,255,0),
+    (255,0,0)
+    ]
+
+
+def mask2contour(mask, width=1):
+    # CONVERT MASK TO ITS CONTOUR
+    w = mask.shape[1]
+    h = mask.shape[0]
+    mask2 = np.concatenate([mask[:,width:],np.zeros((h,width))],axis=1)
+    mask2 = np.logical_xor(mask,mask2)
+    mask3 = np.concatenate([mask[width:,:],np.zeros((width,w))],axis=0)
+    mask3 = np.logical_xor(mask,mask3)
+    return np.logical_or(mask2,mask3) 
+
+
+class MixinMeta():
+    namespace = 'visualize'
+
+
+class Inference(Task, MixinMeta):
+    uploaded_images = Option(type='upload')
+    output_activation = Option(name='activation', type='collection', default='None', collection=[['None', 'sigmoid', 'softmax']])
+
+    transform = DataflowModule(name='Transform', component_types=['Dataflow'], multiple=True, required=False)
+    model = BasicModule(name='Model', component_types=['Model'])
+    
+    def __call__(self):
+        fnames = [i.split('/')[-1] for i in self.uploaded_images]
+        uploaded_images_arrays = [cv2.imread(img) for img in self.uploaded_images]
+        transformed_arrays = [self.transform(image) for image in uploaded_images_arrays]
+        outputs = [self.model(input.unsqueeze(0)).squeeze() for input in transformed_arrays]
+        shape = outputs[0].shape
+        if self.output_activation == 'None':
+            results = outputs
+        elif self.output_activation == 'sigmoid':
+            results = [torch.sigmoid(output) for output in outputs]
+        elif self.output_activation == 'softmax':
+            results = [torch.nn.Softmax()(output) for output in outputs]
+        else:
+            env.logger.exception(f'unexpected error in inferencing process.')
+
+        for idx, image_path in enumerate(self.uploaded_images):
+            # DISPLAY IMAGES WITH DEFECTS
+            plt.figure(figsize=(0.01 * shape[1], 0.01 * shape[0]))
+            img = Image.open(image_path)
+            img_array = np.array(img)
+            patches = []
+            for classes in range(len(results)):
+                try:
+                    msk = results[classes]
+                except:
+                    msk = np.zeros(shape[1:3])
+
+                msk = mask2contour(msk,width=2)
+
+                img_array[msk==1,0] = colors[i][0]
+                img_array[msk==1,1] = colors[i][1]
+                img_array[msk==1,2] = colors[i][2]
+                patches.append(mpatches.Patch(color=matplotlib.colors.to_rgba(np.array(colors[i])/255), label=classes))
+
+            plt.legend(handles=patches)
+            plt.axis('off') 
+            plt.imshow(img_array)
+            plt.subplots_adjust(wspace=0.05)
+            plt.savefig(fnames)
+            env.rpc.add_file(fnames)
